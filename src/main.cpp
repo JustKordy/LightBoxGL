e270c36ee103925ae3a339cc93320c1d38c1930b
JustKordy <honajsek@seznam.cz>
2025-02-05 20:51:05 +0100

    idk

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <glm/glm.hpp>
+#include <gl/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main()
+{
+    if (!glfwInit())
+    {
+        return -1;
+    }
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+}
